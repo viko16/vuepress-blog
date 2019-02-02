4d2a23b0ded6285b296e3ee0832f980690850806
viko16 <16viko@gmail.com>
2019-02-02 14:56:32 +0800

    init

---
+++ posts/.vuepress/config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  title: 'Unknown Me',
+  dest: path.resolve(__dirname, '../../dist'),
+  theme: 'simple',
+  locales: {
+    '/': { lang: 'zh-CN' },
+  },
+}
