487603f094b9575e4d72688b6ee3a6b8db77aaff
viko16 <16viko@gmail.com>
2019-02-03 16:04:51 +0800

    add vuepress config

--- posts/.vuepress/config.js
+++ posts/.vuepress/config.js
@@ -2,9 +2,12 @@ const path = require('path');
 
 module.exports = {
   title: 'Unknown Me',
+  description: 'Viko\'s Blog',
   dest: path.resolve(__dirname, '../../dist'),
-  theme: 'simple',
+  evergreen: true,
   locales: {
     '/': { lang: 'zh-CN' },
   },
+  theme: 'simple',
+  plugins: ['permalink-pinyin']
 }
