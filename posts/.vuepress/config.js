db7eaf071eb343901b73209f5100a5102e49fe3f
viko16 <16viko@gmail.com>
2019-02-03 16:23:39 +0800

    add assets file

--- posts/.vuepress/config.js
+++ posts/.vuepress/config.js
@@ -8,6 +8,11 @@ module.exports = {
   locales: {
     '/': { lang: 'zh-CN' },
   },
+  head: [
+    ['link', { rel: 'shortcut icon', href: '/favicon.png' }],
+    ['meta', { name: 'keywords', content: 'Unknow Me, viko16, vuepress-theme-simple, 博客, vue' }],
+    ['meta', { name: 'author', content: 'viko16' }],
+  ],
   theme: 'simple',
   plugins: ['permalink-pinyin']
 }
