f7c78cd040fcb5fb39ef1da77a93a3835e2a2cf6
viko16 <16viko@gmail.com>
2020-03-15 20:45:17 +0800

    no longer specify publishing time

--- add.sh
+++ add.sh
@@ -4,9 +4,8 @@
 read -p "Enter post title: " title
 
 # variables
-now=$(date "+%Y-%m-%d")
 filename="./posts/$title.md"
 
 # output
-echo "---\ntitle: $title\ndate: $now\n---\n" > $filename
+echo "---\ntitle: $title\n---\n" > $filename
 echo "created $filename"
